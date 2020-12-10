a111b0db6a9fc4cb0287df2b17c639395779c6a5
Pugsly99 <rylandadams@yahoo.com>
2020-12-10 11:47:13 -0800

    built out file structure Co-authored-by: Shannon Grantski <grantski@pm.me>

---
+++ DictionaryTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Scrabble;
+
+namespace Scrabble.Tests
+{
+  [TestClass]
+  public class DictionaryTests
+  {
+    [TestMethod]
+    public void ScoreDictionary_ReturnsExistenceOfDictionary_True()
+    {
+      scoreDictionary
+      // Assert.AreEqual( Expected result, code to test)
+    //  
+    //  public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    // {
+    //   Item newItem = new Item("test");
+    //   Assert.AreEqual(typeof(Item), newItem.GetType());
+    // }
+
+  }
+}
